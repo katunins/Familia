feb8491fd3d8eee9c804a3da5a8968ce2a43d31e
Pavel Katunin <katunin.pavel@gmail.com>
2021-11-13 03:23:49 +0300

    Новое имя

--- ios/familbook/AppDelegate.m
+++ ios/familbook/AppDelegate.m
@@ -38,7 +38,7 @@ static void InitializeFlipper(UIApplication *application) {
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"grandAlbum"
+                                                   moduleName:@"familbook"
                                             initialProperties:nil];
 
   if (@available(iOS 13.0, *)) {
--- ios/familbookTests/familbookTests.m
+++ ios/familbookTests/familbookTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface grandAlbumTests : XCTestCase
+@interface familbookTests : XCTestCase
 
 @end
 
-@implementation grandAlbumTests
+@implementation familbookTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
